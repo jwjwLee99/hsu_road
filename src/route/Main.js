31d1809936eed2fb814bb163050c8d9420f41875
jwjwLee99 <77048470+jwjwLee99@users.noreply.github.com>
2021-09-08 17:04:01 +0900

    Update Main.js

--- src/route/Main.js
+++ src/route/Main.js
@@ -15,7 +15,7 @@ const Main = () => {
       </h1>
       <footer className="mainFooter">
         <h4>제작자 : ㅎㄱㅋㅍㅌ 201800016</h4>
-        <div>건의 및 문의, 오류관련 : 201800016@hanseo.ac.kr</div>
+        <div>건의 및 문의, 오류관련 : ljwwnd99@gmail.com</div>
         <div>포트폴리오용</div>
       </footer>
     </div>
